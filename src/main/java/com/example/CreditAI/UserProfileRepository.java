b61be0d9c85f9962d9698d19d2858bb57a27aacc
animesh <sethanimesh@hotmail.com>
2024-08-20 11:07:20 +0530

    user repository
    repository is used to manage data persistence and retrieval in a database

---
+++ src/main/java/com/example/CreditAI/UserProfileRepository.java
@@ -0,0 +1,10 @@
+package com.example.CreditAI;
+
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long>{
+	Optional<User> findByUsername(String username);
+}
