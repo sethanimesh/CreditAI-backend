3482f2d71b3857e8d3b5e643b2c79409c9b53ff4
animesh <sethanimesh@hotmail.com>
2024-08-20 10:48:39 +0530

    spring initialisation files

---
+++ src/main/java/com/example/CreditAI/CreditAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CreditAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CreditAI/CreditAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
